7084d9906488b1cbfa16c04a3ebfc8a9c01cc007
Peter Nistrup <peter.nistrup@gmail.com>
2018-10-12 10:45:33 +0200

    Update PlayerCell.swift

--- Roskilde 2018/PlayerCell.swift
+++ Roskilde 2018/PlayerCell.swift
@@ -1,11 +1,3 @@
-//
-//  PlayerCell.swift
-//  YouTube Example
-//
-//  Created by Sean Allen on 4/9/17.
-//  Copyright © 2017 Sean Allen. All rights reserved.
-//
-
 import UIKit
 
 class PlayerCell: UITableViewCell {
