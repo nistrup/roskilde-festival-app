0bba874d3ef9323ea46c70e485976e7fbe279217
Peter Nistrup <peter.nistrup@gmail.com>
2018-10-12 10:44:28 +0200

    Update AppDelegate.swift

--- Roskilde 2018/AppDelegate.swift
+++ Roskilde 2018/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  YouTube Example
-//
-//  Created by Sean Allen on 4/9/17.
-//  Copyright © 2017 Sean Allen. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
