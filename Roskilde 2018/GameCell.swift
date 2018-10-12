0d246689d64c145d0f68864ef5a55613339acc88
Peter Nistrup <peter.nistrup@gmail.com>
2018-10-12 10:44:15 +0200

    Update GameCell.swift

--- Roskilde 2018/GameCell.swift
+++ Roskilde 2018/GameCell.swift
@@ -1,13 +1,4 @@
-//
-//  GameCell.swift
-//  YouTube Example
-//
-//  Created by Sean Allen on 4/9/17.
-//  Copyright © 2017 Sean Allen. All rights reserved.
-//
-
 import UIKit
-
 
 class GameCell: UITableViewCell {
     
